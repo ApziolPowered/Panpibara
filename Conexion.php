141c2c5a9b43bee370a2b2ea9f43a24ea9ce45ab
Apziol <axjoheal@gmail.com>
2025-04-02 23:10:52 -0600

    Archivos completos

---
+++ Conexion.php
@@ -0,0 +1,34 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "login_db";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn) 
+{
+	die("No hay conexión: ".mysqli_connect_error());
+}
+
+$nombre = $_POST["txtusername"];
+$pass = $_POST["txtpassword"];
+
+$query = mysqli_query($conn,"SELECT * FROM login WHERE usuario = '".$nombre."' and password = '".$pass."'");
+$nr = mysqli_num_rows($query);
+
+if($nr == 1)
+{
+	header("Location: pagina.html")
+	echo "Bienvenido:" .$nombre;
+}
+else if ($nr == 0) 
+{
+	//header("Location: login.html");
+	//echo "No ingreso"; 
+	echo "<script> alert('Error');window.location= 'login.html' </script>";
+}
+	
+
+
+?>
